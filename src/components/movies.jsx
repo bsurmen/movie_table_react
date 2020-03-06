2360a79cf8999d2c48361b21c74e01484ddf6556
Burak Surmen <busurmen@gmail.com>
2020-03-06 16:32:26 +0300

    Added sorting movie genres

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import { getMovies } from "../services/fakeMovieService";
-import Like from "./common/like"
+import { getGenres } from "../services/fakeGenreService";
+import Like from "./common/like";
+import ListGroup from "./common/listGroup";
+import Pagination from "./common/pagination";
+import { paginate } from "../utils/paginate"
+
 
 class Movies extends Component {
-    state = { movies: getMovies() }
+    state = {
+        movies: [],
+        genres: [],
+        pageSize: 4,
+        currentPage: 1
+    }
 
-
+    componentDidMount() {
+        const genres = [{ name: "All Genres" }, ...getGenres()]
+        this.setState({ movies: getMovies(), genres })
+    }
 
     handleDelete = (movie) => {
         const movies = this.state.movies.filter(m => m._id !== movie._id)
@@ -23,10 +36,32 @@ class Movies extends Component {
 
     }
 
+    handlePageChange = (page) => {
+        this.setState({
+            currentPage: page
+        })
+    }
+
+
+    handleGenreSelect = (genre) => {
+        this.setState({
+            selectedGenre: genre,
+            currentPage: 1
+        })
+
+    }
 
     render() {
-        const { length: count } = this.state.movies;
 
+        const { length: count } = this.state.movies;
+        const { pageSize, currentPage, selectedGenre, movies: allMovies } = this.state;
+
+        const filtered = selectedGenre && selectedGenre._id ? allMovies.filter(m => m.genre._id === selectedGenre._id) : allMovies;
+
+        const movies = paginate(filtered, currentPage, pageSize);
+
+
+        if (count === 0) return <p>There are no movies in the database.</p>
         const table = <table className="table">
             <thead>
                 <tr>
@@ -37,7 +72,7 @@ class Movies extends Component {
                 </tr>
             </thead>
             <tbody>
-                {this.state.movies.map(movie => (<tr key={movie._id}>
+                {movies.map(movie => (<tr key={movie._id}>
                     <td>{movie.title}</td>
                     <td>{movie.genre.name}</td>
                     <td>{movie.numberInStock}</td>
@@ -49,12 +84,27 @@ class Movies extends Component {
             </tbody>
         </table>;
 
-        if (count === 0) return <p>There are no movies in the database.</p>
         return (
-            <>
-                <p>Showing {count} movies in the database.</p>
-                {table}
-            </>
+            <div className="row">
+                <div className="col-3">
+                    <ListGroup
+                        items={this.state.genres}
+                        onItemSelect={this.handleGenreSelect}
+                        selectedItem={this.state.selectedGenre}
+                    />
+                </div>
+                <div className="col">
+                    <p>Showing {filtered.length} movies in the database.</p>
+                    {table}
+                    <Pagination
+                        itemsCount={filtered.length}
+                        pageSize={pageSize}
+                        currentPage={currentPage}
+                        onPageChange={this.handlePageChange}
+                    />
+                </div>
+
+            </div>
         );
     }
 }
